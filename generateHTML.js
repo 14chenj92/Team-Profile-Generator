577b285023842439a6cfd83901a8c7bf4c24f913
J <09.chen@gmail.com>
2023-02-26 13:09:03 -0800

    shortened code + format fixes

--- generateHTML.js
+++ generateHTML.js
@@ -1,21 +1,16 @@
-const Intern = require('./lib/Intern');
-const Manager = require('./lib/Manager');
-const Engineer = require('./lib/Engineer');
-const Employee = require('./lib/Employee');
-
 
 const managercard = data => { return data.map(manager => {
   return `
-  <div class="col-4 mt-4">
+  <div class="col-3 mt-4">
   <div class="card" style="width: 18rem;">
           <div class="card-header">
               <h3>${manager.name}</h3>
               <h4>Manager</h4> <img src="./images/manager.jpg" /></a>
           </div>
-          <div class="card-body">
-              <p class="id">ID: ${manager.id}</p>
-              <p class="email">Email: <a href="mailto:">${manager.email}</a></p>
-              <p class="office">Office Number: ${manager.officenumber}</p>
+          <div class="body">
+              <p>ID: ${manager.id}</p>
+              <p>Email: <a href="mailto:">${manager.email}</a></p>
+              <p>Office Number: ${manager.officenumber}</p>
           </div>
       </div>
   </div>
@@ -24,16 +19,16 @@ const managercard = data => { return data.map(manager => {
 
 const engineercard = data => { return data.map(engineer => {
   return `
-  <div class="col-4 mt-4">
+  <div class="col-3 mt-4">
   <div class="card" style="width: 18rem;">
           <div class="card-header">
               <h3>${engineer.name}</h3>
               <h4>Engineer</h4> <img src="./images/engineer.png" /></a>
           </div>
-          <div class="card-body">
-              <p class="id">ID: ${engineer.id}</p>
-              <p class="email">Email: <a href="mailto:">${engineer.email}</a></p>
-              <p class="github">Github: ${engineer.github}</p>
+          <div class="body">
+              <p>ID: ${engineer.id}</p>
+              <p>Email: <a href="mailto:">${engineer.email}</a></p>
+              <p>Github: <a href="https://github.com/${engineer.github}">${engineer.github}</a></p>
           </div>
       </div>
   </div>
@@ -42,31 +37,31 @@ const engineercard = data => { return data.map(engineer => {
 
 const interncard = data => { return data.map(intern => {
   return `
-  <div class="col-4 mt-4">
+  <div class="col-3 mt-4">
   <div class="card" style="width: 18rem;">
           <div class="card-header">
               <h3>${intern.name}</h3>
               <h4>Intern</h4> <img src="./images/intern.png" /></a>
           </div>
-          <div class="card-body">
-              <p class="id">ID: ${intern.id}</p>
-              <p class="email">Email: <a href="mailto:">${intern.email}</a></p>
-              <p class="school">School: ${intern.school}</p>
+          <div class="body">
+              <p>ID: ${intern.id}</p>
+              <p>Email: <a href="mailto:">${intern.email}</a></p>
+              <p>School: ${intern.school}</p>
           </div>
       </div>
   </div>
   `
 })}
 
-generateHTML = (teamArray) => {
+generateHTML = (cardarray) => {
   let cards = [];
-  const manager = teamArray.filter(data => {
+  const manager = cardarray.filter(data => {
       return data.getRole() === 'Manager';
   });
-  const engineer = teamArray.filter(data => {
+  const engineer = cardarray.filter(data => {
       return data.getRole() === 'Engineer';
   });
-  const intern = teamArray.filter(data => {
+  const intern = cardarray.filter(data => {
       return data.getRole() === 'Intern';
   });
   if (manager) {
